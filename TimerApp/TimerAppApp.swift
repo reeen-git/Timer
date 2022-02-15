7433e5a861701796bcd3e2db28861fa00b37a742
Ren <ren.t21@icloud.com>
2022-02-15 21:38:46 +0900

    Initial Commit

---
+++ TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by 高橋蓮 on 2022/02/15.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
